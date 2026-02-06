26f46e3c6c42acefbade1f5305ad8ea0d3712cf0
AnFremd <112823576+AnFremd@users.noreply.github.com>
2026-02-07 00:50:38 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0, backgroundColor: 'black' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
