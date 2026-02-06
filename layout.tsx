d1be79cff30d384fdc576143f513d2c901df5844
AnFremd <112823576+AnFremd@users.noreply.github.com>
2026-02-07 00:50:22 +0700

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ru">
-      <body style={{ margin: 0, backgroundColor: 'black' }}>{children}</body>
-    </html>
-  )
-}
